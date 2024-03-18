905782b9f5af9964ea399606a4c8d7144c0932af
GabrielRonek <s24515@pjwstk.edu.pl>
2024-03-18 19:54:13 +0100

    dodano liczenie sredniej

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,15 @@ namespace ConsoleApp1
         static void Main(string[] args){
            
             int[] a = [-12, 6, -99, 2, 14, -4, 2];
-            
+            countAv(a);
         }  
+        static void countAv(int[] b){
+            float sum = 0;
+            for(int a = 0; a < b.Length; a++){
+                sum += b[a];
+            }
+            Console.WriteLine("The average of the given array is: {0}", sum/b.Length);
         
+        }
     }
 }
